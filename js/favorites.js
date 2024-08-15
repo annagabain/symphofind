8e4f0fe24e6ad7ec8fc1567aa7d9c89b84407db6
AG <anna.gabain@gmail.com>
2024-08-15 12:09:23 +0200

    Test displaying information about favourites from the local storage to DOM

--- js/favorites.js
+++ js/favorites.js
@@ -1,10 +1,55 @@
+
+
 // TESTS
-setItem('testComposerCard', 'test composer card content');
+// ---------------------------------------------------
+// ---------------------------------------------------
+
+
+setItem("testComposerCard", "test composer card");
 // test getting a composer card by key
-console.log(getItem('testComposerCard'));
+console.log(getItem("testComposerCard"));
+
+
+// ---------------------------------------------------
+// ---------------------------------------------------
+
+
+function addToFavorites(composer) {
+  const favorites = JSON.parse(localStorage.getItem("testComposerCard")) || [];
+
+  if (!favorites.includes(composer)) {
+    favorites.push(composer);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }
+}
+
+// PLACEHOLDER
+// function removeFromFavorites(composer)
+
+
+// PLACEHOLDER
+// check if a composer is in favorites
+// function isFavorite(composer)
 
 
 
+// TEST
+// load favorite composers from localStorage
+function loadFavorites() {
+  const favorites = JSON.parse(localStorage.getItem("testComposerCard")) || [];
+  const favoritesList = document.querySelector(".favorites-list");
 
+  favoritesList.innerHTML = "";
 
+  favoritesList.innerHTML = `
 
+            ${favorites}
+        `;
+}
+
+// PLACEHOLDER
+// Event listener for the favorite toggle buttons
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadFavorites();
+});
